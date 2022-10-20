f0ecf4b7bc483c6e3594b91a19479488ba3988b9
Malhar <malharkulkarni19@gmail.com>
2022-10-20 23:34:11 +0530

    Basic React Form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import './index.css';
 
 function App() {
+  const[values, setValues] = useState({
+    firstName : "",
+    lastName : "",
+    email : "",
+  })
+
+  const [submit, setSubmit] = useState(false);
+  const [valid, setValid] = useState(false);
+
+  const handleFirstNameInputChange = (event) => {
+    setValues({...values, firstName: event.target.value})
+  } 
+  const handleLastNameInputChange = (event) => {
+    setValues({...values, lastName: event.target.value})
+  } 
+  const handleemailInputChange = (event) => {
+    setValues({...values, email: event.target.value})
+  } 
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if(values.firstName &&  values.lastName && values.email){
+      setValid(true);
+    }
+    setSubmit(true);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="form-container">
+      <form className="register-form" onSubmit={handleSubmit}>
+      {submit && valid ? <div className='success-message'>Registration Successful!</div> : null} 
+        <input type="text" className="form-field" id="first-name" placeholder="First Name" value={values.firstName} onChange={handleFirstNameInputChange}/>{submit && !values.firstName ? <span>please enter name</span> : null}
+        <input type="text" className="form-field" id="last-name" placeholder="Last Name" value={values.lastName} onChange={handleLastNameInputChange}/>{submit && !values.lastName ? <span>please enter last name</span> : null}
+        <input type="text" className="form-field" id="email" placeholder="E-mail" value={values.email} onChange={handleemailInputChange}/>{submit && !values.email ? <span>please enter email</span> : null}
+        <button type="submit" className="form-field">Submit</button>
+      </form>
     </div>
   );
 }
